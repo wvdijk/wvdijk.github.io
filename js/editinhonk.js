77fffb3c0d4c044990432795ca972cc259b0805a
Wieland van Dijk <wvdijk@gmail.com>
2019-06-23 21:26:02 +0200

    Edit in Honk if bookmarklet unavailable

---
+++ js/editinhonk.js
@@ -0,0 +1,10 @@
+document.getElementById('submit').onclick = function () {
+	let artURL = document.getElementById('artURL').value;
+	let artID = artURL.match(/\-a(\d+)/)[1];
+	let editlink = document.createElement('a');
+	editlink.href = `https://honk.nrc.nl/bvhw/?nrc_action=hub2import&article_id=${artID}`;
+	let linktxt = document.createTextNode('Edit in Honk, at your own risk');
+	editlink.appendChild(linktxt);
+	document.body.appendChild(editlink);
+	return false;
+}
