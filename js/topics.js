5a28d1d021dbc3dbfaa43549b463fb07d3a00c3b
Wieland van Dijk <wvdijk@gmail.com>
2019-07-30 14:04:43 +0200

    Got rid of redundant var

--- js/topics.js
+++ js/topics.js
@@ -1,6 +1,5 @@
 javascript: (function(){
-  var pagetitle = document.title;
-  let headline = pagetitle.split(" | ")[0];
+  let headline = document.title.split(" | ")[0];
   let searchString = encodeURIComponent(headline);
   let url = 'http://www.topics.nl/zoek/?query=' + searchString;
   window.open(url, '_blank');
