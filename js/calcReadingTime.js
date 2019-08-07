ba8913cf4eed33ee8a67d0cf7f6b36335fa69a66
Wieland van Dijk <wvdijk@gmail.com>
2019-08-07 11:11:14 +0200

    calcReadingTime initial commit

---
+++ js/calcReadingTime.js
@@ -0,0 +1,16 @@
+let calcReadingTime = function(elem) {
+	let words = elem.textContent.split(' ');
+	let seconds = (words.length / 300) * 60;
+	let numImages = elem.querySelector('img').length;
+	for (let i = 0; i < numImages; i++) {
+		if (i > 9) {
+			seconds += 3;
+		} else {
+			seconds += 12 - i;
+		}
+	}
+	return Math.round(seconds);
+};
+
+let articleBody = document.getElementsByClassName('article__content')[0];
+alert(`De berekende leestijd voor dit artikel is ${calcReadingTime(articleBody)} seconden`);
