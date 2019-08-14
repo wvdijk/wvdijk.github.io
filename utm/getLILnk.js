a64bcc0ad8fda61c2bbca66936c3b228ee3fe195
Wieland van Dijk <wvdijk@gmail.com>
2019-08-14 13:51:13 +0200

    Fixed redeclaration err on multiple use

--- utm/getLILnk.js
+++ utm/getLILnk.js
@@ -1,12 +1,14 @@
-let getLILnk = function() {
-	let url = window.location.toString().split('?')[0].split('#')[0];
-	let date = new Date;
-	let y = date.getFullYear();
-	let m = date.getMonth() + 1;
-	m < 10 ? m = `0${m}` : m = m;
-	let d = date.getDate();
-	d < 10 ? d = `0${d}` : d = d;
-	let utm = `${url}?utm_source=social&utm_medium=linkedin&utm_campaign=linkedin&utm_term=${y}${m}${d}`;
-	prompt('Gebruik deze link in je post op LinkedIn:', utm);
-	};
+if (!getLILnk) {
+	var getLILnk = function() {
+		let url = window.location.toString().split('?')[0].split('#')[0];
+		let date = new Date;
+		let y = date.getFullYear();
+		let m = date.getMonth() + 1;
+		m < 10 ? m = `0${m}` : m = m;
+		let d = date.getDate();
+		d < 10 ? d = `0${d}` : d = d;
+		let utm = `${url}?utm_source=social&utm_medium=linkedin&utm_campaign=linkedin&utm_term=${y}${m}${d}`;
+		prompt('Gebruik deze link in je post op LinkedIn:', utm);
+	}
+};
 getLILnk();
