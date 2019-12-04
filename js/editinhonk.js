0e03a474167e2439e97a4ed8875997e4482bee26
Wieland van Dijk <wvdijk@gmail.com>
2019-12-04 11:38:52 +0100

    minor improv to editinhonk

--- js/editinhonk.js
+++ js/editinhonk.js
@@ -3,7 +3,7 @@ document.getElementById('submit').onclick = function () {
 	let artID = artURL.match(/\-a(\d+)/)[1];
 	let editlink = document.createElement('a');
 	editlink.href = `https://honk.nrc.nl/bvhw/?nrc_action=hub2import&article_id=${artID}`;
-	let linktxt = document.createTextNode('Edit in Honk, at your own risk');
+	let linktxt = document.createTextNode(`Edit in Honk, at your own risk: ${artID}\n`);
 	editlink.appendChild(linktxt);
 	document.body.appendChild(editlink);
 	return false;
