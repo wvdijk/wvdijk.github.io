60ce57ab38b899b7447801ce43da15cad987571e
Wieland van Dijk <wvdijk@gmail.com>
2020-09-02 22:38:39 +0200

    Improved image detection

--- js/getJpeg.js
+++ js/getJpeg.js
@@ -1,5 +1,4 @@
-let img = document.querySelector('figure.article__featured-image div.responsive-img-div img').src;
+let img = document.querySelector('meta[property="og:image"]').content;
 const REGEX = /(?<=s3\/)static\.nrc\.nl.*$/
 const JPG = `https://${img.match(REGEX)}`;
 window.open(JPG);
-
