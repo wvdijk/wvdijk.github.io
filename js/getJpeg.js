a5555ae0a8a24cc7d9b9f6827baea3e0103980ff
Wieland van Dijk <wvdijk@gmail.com>
2020-09-05 07:49:12 +0200

    Fixed: works only on first use

--- js/getJpeg.js
+++ js/getJpeg.js
@@ -1,4 +1,6 @@
-let img = document.querySelector('meta[property="og:image"]').content;
-const REGEX = /(?<=s3\/)static\.nrc\.nl.*$/
-const JPG = `https://${img.match(REGEX)}`;
+if (!JPG) {
+    let img = document.querySelector('meta[property="og:image"]').content;
+    const REGEX = /(?<=s3\/)static\.nrc\.nl.*$/
+    const JPG = `https://${img.match(REGEX)}`;
+}
 window.open(JPG);
