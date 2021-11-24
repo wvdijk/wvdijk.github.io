aa74c821c595328d3814c14a84626f48d2c87b1a
Wieland van Dijk <wvdijk@gmail.com>
2021-11-24 13:18:32 +0100

    Fix for UTM on Insta Stories

--- utm/getInstaStoryLnk.js
+++ utm/getInstaStoryLnk.js
@@ -1,13 +1,7 @@
 if (!getInstaStoryLnk) {
 	var getInstaStoryLnk = function() {
 		let url = window.location.toString().split(/[#\?]+/)[0];
-		let date = new Date;
-		let y = date.getFullYear();
-		let m = date.getMonth() + 1;
-		m < 10 ? m = `0${m}` : m = m;
-		let d = date.getDate();
-		d < 10 ? d = `0${d}` : d = d;
-		let utm = `${url}?utm_source=instagram&utm_medium=social&utm_campaign=stories&utm_term=${y}${m}${d}`;
+		let utm = `${url}/utm/igstory`;
 		prompt('Gebruik deze link in je story op Instagram:', utm);
 	}
 };
